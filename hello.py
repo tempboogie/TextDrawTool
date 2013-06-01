134c35601d048dc55d25e50deffbb43d434c758b
MrLook <vincent.look@gmail.com>
2013-05-31 23:14:26 -0700

    created
    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print ("helloworld")
+
+#open a file
+myfile = open('test.txt','w')
+
+
+#process request
+myfile.write("This is my first text file written in python\n")
+
+#close file
+myfile.close()
